be4c033ecc2dd26a731a9c6cf7d9b8143b76a66c
unknown <shlimon@deakin.edu.au>
2022-08-07 20:49:14 +1000

    redoing the latest component of the blog section for mobile view

--- src/components/Blog/Latest.vue
+++ src/components/Blog/Latest.vue
@@ -2,28 +2,34 @@
 <section class="latest">
   <h3>Latest Posts</h3>
   <div class="posts">
-    <div class="post">
-      <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/smart-city.jpg')}')` }"></div>
+    <div class="post box1">
+      <!-- <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/smart-city.jpg')}')` }"></div> -->
+      <img class="post__image":src="smartCity" alt="smart city">
       <h4>Smart City</h4>
     </div>
-    <div class="post coll-2 roww-2">
-      <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/ev-charger.jpg')}')` }"></div>
+    <div class="post coll-2 roww-2 box2">
+      <!-- <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/ev-charger.jpg')}')` }"></div> -->
+      <img class="post__image":src="evCharger" alt="ev charger">
       <h4>EV Charger</h4>
     </div>
-    <div class="post roww-2">
-      <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/chameleon-web.jpg')}')` }"></div>
+    <div class="post roww-2 box3">
+      <!-- <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/chameleon-web.jpg')}')` }"></div> -->
+      <img class="post__image":src="chameleonWeb" alt="chameleonWeb">
       <h4>Chameleon Website</h4>
     </div>
-    <div class="post roww-2">
-      <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/smart-data.jpg')}')` }"></div>
+    <div class="post roww-2 box4">
+      <!-- <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/smart-data.jpg')}')` }"></div> -->
+      <img class="post__image":src="smartData" alt="smartData">
       <h4>Smart Data</h4>
     </div>
     <div class="post">
-      <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/Deakin.jpg')}')` }"></div>
+      <img class="post__image":src="Deakin" alt="Deakin">
+      <!-- <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/Deakin.jpg')}')` }"></div> -->
       <h4>Deakin</h4>
     </div>
     <div class="post coll-2">
-      <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/sit-class.jpg')}')` }"></div>
+      <img class="post__image":src="sit" alt="sit">
+      <!-- <div class="image" :style="{ backgroundImage: `url('${require('@/assets/images/blogImage/sit-class.jpg')}')` }"></div> -->
       <h4>SIT374</h4>
     </div>
   </div>
@@ -32,47 +38,74 @@
 
 <script>
 export default {
-  name: 'latest'
+  name: 'latest',
+  data(){
+    return{
+      smartCity:require('@/assets/images/blogImage/smart-city.jpg'),
+      evCharger:require('@/assets/images/blogImage/ev-charger.jpg'),
+      chameleonWeb: require('@/assets/images/blogImage/chameleon-web.jpg'),
+      smartData: require('@/assets/images/blogImage/smart-data.jpg'),
+      Deakin:require('@/assets/images/blogImage/Deakin.jpg'),
+      sit: require('@/assets/images/blogImage/sit-class.jpg'),
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .latest {
     display: block;
     width: 100%;
     max-width: 1280px;
     margin: 0 auto;
-    .posts {
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(3, 196px);
-        grid-gap: 30px;
-        margin: 15px;
-        .post {
-            display: flex;
-            flex-direction: column;
-            background-color: #FFFFFF;
-            box-shadow: 3px 3px 8px 0 rgba(0, 0, 0, 0.2);
-            .image {
-                flex: 1;
-                background-position: center;
-                background-size: cover;
-                background-repeat: no-repeat;
-            }
-            h4 {
-                box-sizing: border-box;
-                padding: 3px 15px 0;
-                height: 50px;
-                line-height: 50px;
-                margin: 0;
-            }
-            &.coll-2 {
-                grid-column: span 2;
-            }
-            &.roww-2 {
-                grid-row: span 2;
-            }
-        }
-    }
+  }
+.posts{
+  display:grid;
+  grid-template-columns:repeat(4,1fr);
+  grid-template-rows:repeat(3, 0.5fr);
+  grid-gap: 30px;
+  margin: 15px;
+
 }
+
+
+
+.post{
+  /* background-color: steelblue; */
+  display: flex;
+  flex-direction: column;
+  box-shadow: 3px 3px 8px 0 rgba(0, 0, 0, 0.2);
+  align-items: center;
+}
+
+.post__image{
+  flex: 1;
+  width:100%;
+
+  /* background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat; */
+
+}
+
+/* .box1{
+  grid-column: 2/4;
+} */
+
+.box2{
+  grid-column: 2/4;
+}
+
+.box4{
+  grid-row: 2/4;
+
+}
+
+
+h4{
+  font-size:1rem;
+  margin:0;
+}
+
+
 </style>
