ea98088931c83bf09c485ff654bb4fe1691acfc1
chenyukong <kongc@deakin.edu.au>
2022-09-08 15:03:52 +1000

    Portfolio

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,54 +1,117 @@
 <template>
   <div class="main">
+    <Head></Head>
     <div class="fir">
       <img :src= "melbourneCity" alt="melboure" />
       <div class="bottom-left">
-        <h1>SMART CITIES OPEN DATA</h1>
+        <h1>{{firstHeading}}</h1>
       </div>
       <div class="read-more">
-        <button type="read-more" name="button">Read More</button>
+        <router-link to="/web/portfolioP1" v-slot="{href, navigate}">
+          <button :href="href" @click="navigate" class='whatever-you-want'>
+            Read More
+          </button>
+        </router-link>
       </div>
     </div>
 
     <div class="fir" >
       <img :src= "ev" alt="ev charging" />
       <div class="bottom-right" style = "right: 5px" >
-        <h1>EV CHARGER PLACEMENT</h1>
+        <h1>{{secondHeading}}</h1>
       </div>
       <div class="read-more-left">
-        <button type="read-more" name="button">Read More</button>
+        <router-link to="/web/portfolioP2" v-slot="{href, navigate}">
+          <button :href="href" @click="navigate" class='whatever-you-want'>
+            Read More
+          </button>
+        </router-link>
+        <!-- <button type="read-more" name="button">Read More</button> -->
       </div>
     </div>
 
     <div class="fir" >
       <img :src= "suburb" alt="melboure" />
       <div class="bottom-left">
-        <h1>LOCATE A SOCKET</h1>
+        <h1>{{thirdHeading}}</h1>
       </div>
       <div class="read-more">
-        <button type="read-more" name="button">Read More</button>
+        <router-link to="/web/portfolioP3" v-slot="{href, navigate}">
+          <button :href="href" @click="navigate" class='whatever-you-want'>
+            Read More
+          </button>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// const melbourneCity = require("@/assets/images/melbourne.jpg")
-
+const Logo= require("@/assets/images/logo2.png");
+const Baob= require("@/assets/images/menu.png");
+import Head from "./Head.vue"
 export default {
   name: "Portfolio",
   data() {
     return {
       melbourneCity:require("@/assets/images/melbourne.jpg"),
       ev:require("@/assets/images/ev.jpg"),
-      suburb:require("@/assets/images/suburb.jpg")
-
+      suburb:require("@/assets/images/suburb.jpg"),
+      firstHeading:"SMART CITIES OPEN DATA",
+      secondHeading:"EV CHARGER PLACEMENT",
+      thirdHeading:"CHAMELEON WEBSITE",
+      Logo,
+      Baob,
+      isshow:false
     }
+  },
+  methods:{
+     show(){
+      if(this.isshow){
+        this.isshow=false;
+      }else{
+        this.isshow=true;
+      }
+    }
+  },
+  components:{
+    Head
   }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+ .box{
+    float: right;
+    margin-right: 40px;
+
+  }
+.port{
+    position: absolute;
+    right: 10px;
+    top: 50px;
+    z-index: 999;
+  }
+  .port li{
+    list-style: none;
+
+    a{
+      text-decoration: none;
+      color:black;
+    }
+  }
+
+  .logo{
+    width: 80px;
+    height: 80px;
+    margin-left: 10px;
+  }
+  .more{
+  width: 40px;
+  height: 40px;
+  float: right;
+  margin-top: 20px;
+  }
 .fir{
 position: relative;
 text-align:left;
@@ -56,22 +119,22 @@ color: white;
 }
 .fir img {
   width: 100%;
-  height: 900px;
+  height: 200px;
 }
 .bottom-left {
   width: 40%;
   position: absolute;
-  bottom: 180px;
+  bottom: 80px;
   left: 12px;
 }
 .bottom-left h1{
-  font-size : 120px;
+  font-size : 5vw;
   font-weight: bold;
   text-shadow: 2px 2px black;
 }
 .read-more{
 position: absolute;
-bottom:150px;
+bottom:20px;
 left: 12px;
 }
 
@@ -88,21 +151,22 @@ left: 12px;
 }
 
 .bottom-right {
+  width: 40%;
   position: absolute;
-  bottom: 180px;
+  bottom: 80px;
   right: 12px;
   text-align:right;
 }
 
 .bottom-right h1{
-  font-size : 120px;
+  font-size : 5vw;
   font-weight: bold;
   text-shadow: 2px 2px black;
 }
 
 .read-more-left{
 position: absolute;
-bottom:150px;
+bottom:20px;
 right: 10px;
 }
 
---
+++ src/pages/Portfolio3.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="three">
+    <Head></Head>
+      <div class="head">
+
+        <div></div>
+          <div>Chameleon Website Development</div>
+        <div></div>
+      </div>
+      <div class="main">
+        <p>Like any other company, the Chameleon company requires a website to be its digital public facing space. The main goal of the website is to promote Chameleon’s projects, products, and the services that it provides. As the project implementation progresses additional features and functionalities will be added such as blogs, events calendar, and a clients 'area.</p>
+        <p>The goal for this trimester is to :</p>
+        <ul>
+          <li>-	Identify the hosting infrastructure and setup the DevOps pipelines</li>
+          <li>-	Identify the web development tools and SDLC processes to follow</li>
+          <li>-	Propose an initial website layout and style</li>
+          <li>-	Include an initial list of standard pages (including links to other Chameleon projects) and start working on their contents </li>
+          <li>-	The website pages will be static where the content is hardcoded not database driven</li>
+          <li>-	A basic subscribe, announcement and login feature with implementation of security</li>
+          <li>-	User profile section with AI/ML componenet for image recognition</li>
+          <li>-	Basic steps to integrate a blogging platform for users</li>
+        </ul>
+        <div class="video-p1">
+          <iframe id="kaltura_player" src="https://cdnapisec.kaltura.com/p/2006242/sp/200624200/embedIframeJs/uiconf_id/32026212/partner_id/2006242?iframeembed=true&playerId=kaltura_player&entry_id=1_ld34wf56&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_3xwefd5d" width="500" height="281" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *"  frameborder="0" title="Chameleon Website Showcase (T1 2022)"></iframe>
+        </div>
+      </div>
+  </div>
+</template>
+<script>
+const Logo= require("@/assets/images/logo2.png");
+const Baob= require("@/assets/images/menu.png");
+import Head from "./Head.vue"
+export default {
+  name: "PortfolioP3",
+  data() {
+    return{
+       Logo,
+      Baob,
+      isshow:false
+    }
+  },
+    methods:{
+     show(){
+      if(this.isshow){
+        this.isshow=false;
+      }else{
+        this.isshow=true;
+      }
+    }
+  },
+  components:{
+    Head
+  }
+}
+</script>
+<style scoped lang="scss">
+.three {
+   .box{
+    float: right;
+    margin-right: 20px;
+  }
+  .port li{
+    display: inline-block;
+    list-style: none;
+    border: 1px solid black;
+
+    a{
+      text-decoration: none;
+      color:black;
+    }
+  }
+  .port li:hover{
+    background-color: black;
+  }
+  .logo{
+    width: 80px;
+    height: 80px;
+    margin-left: 10px;
+  }
+  .more{
+  width: 40px;
+  height: 40px;
+  float: right;
+  margin-top: 20px;
+  }
+  padding: 40px 40px;
+  .head {
+    display: flex;
+    align-items: center;
+      div:first-child {
+        flex: 1;
+        height: 2px;
+        background: #99c854;
+      }
+      div:nth-child(2) {
+        padding: 0 20px;
+        font-size: 30px;
+        font-weight: bold;
+      }
+      div:last-child {
+        flex: 0;
+        height: 2px;
+        background: #99c854;
+      }
+  }
+  .main {
+    margin-top: 40px;
+    font-size: 24px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+
+      p {
+        font-size: 20px;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+      }
+      ul li {
+        font-size: 20px;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+        list-style-type: none;
+        margin:10px 5px;
+      }
+    .video-p1 {
+      width:100%;
+      min-height:200px;
+      margin-top:40px;
+
+    }
+    .video-p1 iframe {
+      width:100%;
+      min-height:200px;
+    }
+  }
+}
+</style>
